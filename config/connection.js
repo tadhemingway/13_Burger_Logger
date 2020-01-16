78da3be72cd4b06521828cbedd20e4e36f3f2071
Tad83 <tad83hemingway@gmail.com>
2020-01-16 17:24:50 -0600

    Finished the setup of all folders and documents

---
+++ config/connection.js
@@ -0,0 +1,16 @@
+const mysql = require('mysql');
+
+const connection = mysql.createConnection({
+    host: localhost,
+    port: 3306,
+    user: 'root',
+    password: 'Themingway83',
+    database: 'burgers_db'
+});
+
+connection.connect(function(err) {
+if (err) throw err.stack;
+console.log('connected as id: ' + connection.threadId);
+});
+
+module.exports = connection;
---
+++ config/orm.js
@@ -0,0 +1,18 @@
+const connection = require('./connection.js');
+
+
+let orm = {
+    all: function() {
+
+    },
+
+    create: function() {
+
+    },
+
+    update: function() {
+
+    }
+};
+
+module.exports = orm;
